5a2c7d6cb4794215fdedd072ffa268dd7b6c3780
Pintu Sharma <pintu.sharma@empover.com>
2026-01-08 14:42:58 +0530

    Latest code push

---
+++ forloop.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "For Loop"
+#Type One
+for x in 1 2 3 4 5 6 7 8 9
+do
+	echo "Number is $x"
+done
+#Type Two
+for words  in ramu pintu sandhya kajal
+do
+	echo "name is $words"
+done
+
