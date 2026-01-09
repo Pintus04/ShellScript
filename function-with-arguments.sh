24f284ade110bb663466d431aeb5af0bbaea6e11
Pintu Sharma <pintu.sharma@empover.com>
2026-01-09 12:37:35 +0530

    function with arg

---
+++ function-with-arguments.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+function something {
+	echo "..............."
+	echo "name $1"
+	echo "..............."
+}
+
+something vihal
+something vikas
+something rohit
+something gangaj
---
+++ function.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# To make function
+function welcomenote {
+echo "Hello World..!"
+echo "Hello India..!"
+}
+
+# to call our function
+welcomenote
+welcomenote
+welcomenote
