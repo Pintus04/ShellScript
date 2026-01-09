a1115496a779007dfc64b90d8ed978b8627e5456
Pintu Sharma <pintu.sharma@empover.com>
2026-01-09 10:19:56 +0530

    For && By loop

---
+++ infinite_loop.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# this is by loop
+echo "by loop"
+# This Command is printing Hi Buddy every 2 second.
+while true
+do
+	echo "Hi Buddy"
+	sleep 2s
+done
+
+
+# this is for loop
+echo "For Loop"
+for ( ;; )
+do
+	echo "Hello Buddy"
+	sleep 2s
+done
+
+
