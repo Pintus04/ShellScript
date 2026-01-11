c450889b0af4bada3ac034072b9e2a0da3431192
Pintu Sharma <pintu.sharma@empover.com>
2026-01-11 09:52:13 +0530

    Arguments Passing

---
+++ arguments_passing.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# to access the arguments first inter first arguments second inter second arguments
+
+
+echo "First arguments  is $1"
+echo "Second arguments is $2"
+
+
+
+
+
+
+
+
+
+
+
+
