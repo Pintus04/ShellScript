c160950db9d9f0c05b9011d45bf1a00ee8af92f6
PintuSharma <pintusharma0946@gmail.com>
2026-01-27 13:47:23 +0530

    Refactor SSH connection options in Automatic-ssh.sh
    Updated SSH connection options to use a placeholder for pem file path and IP address.

--- Automatic-ssh.sh
+++ Automatic-ssh.sh
@@ -7,14 +7,10 @@ echo "Option c, Connect KCL Backend Server"
 read -p "Select Option-(a, b, c) for Connecting your aws server : " awsserver
 
 password=1234
-echo "$password" | sudo -S apt update -y > testing.sh
-
 
 case $awsserver in
-	a)  sudo ssh -i "/home/pintusharma/Documents/FluxsensT&T-PemFile.pem" ubuntu@13.232.3.197;;
-	b)  sudo ssh -i "/home/pintusharma/Documents/Chatbort-Server.pem"   ubuntu@3.109.18.219;;
-	c)  sudo ssh -i "/home/pintusharma/Documents/KCL-Project.pem" ubuntu3.108.193.21;;
-	D)  echo "Data is not store in Database. Please Connect your SystemAdmin.. Thanks"
+        a)  echo "$password" |  sudo -S ssh -i "Pem file path" ubuntu@IP-ADDRESS;;
+        b)  echo "$password" |  sudo -S ssh -i "Pem file path"   ubuntu@IP-ADDRESS;;
+        c)  echo "$password" |  sudo -S ssh -i "Pem file path" ubuntu@IP-ADDRESS;;
+        D)  echo "Data is not store in Database. Please Connect your SystemAdmin.. Thanks"
 esac
-
-
